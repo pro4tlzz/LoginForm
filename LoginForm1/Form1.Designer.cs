1c069d33879a7ae8f9fcc2106f51b49f60dbef2f
Pro4TLZZ <superfrillz12@hotmail.co.uk>
2018-05-31 19:31:13 +0100

    Add project files.

---
+++ LoginForm1/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace LoginForm
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_userName = new System.Windows.Forms.Label();
+            this.txtbox_userName = new System.Windows.Forms.TextBox();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.lbl_passWord = new System.Windows.Forms.Label();
+            this.txtbox_passWord = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_userName
+            // 
+            this.lbl_userName.AutoSize = true;
+            this.lbl_userName.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_userName.Location = new System.Drawing.Point(12, 65);
+            this.lbl_userName.Name = "lbl_userName";
+            this.lbl_userName.Size = new System.Drawing.Size(182, 37);
+            this.lbl_userName.TabIndex = 0;
+            this.lbl_userName.Text = "Username: ";
+            // 
+            // txtbox_userName
+            // 
+            this.txtbox_userName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox_userName.Location = new System.Drawing.Point(211, 65);
+            this.txtbox_userName.Name = "txtbox_userName";
+            this.txtbox_userName.Size = new System.Drawing.Size(215, 38);
+            this.txtbox_userName.TabIndex = 1;
+            //this.txtbox_userName.TextChanged += new System.EventHandler(this.txtbox_userName_TextChanged);
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.AutoSize = true;
+            this.btn_Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Login.Location = new System.Drawing.Point(164, 248);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(96, 43);
+            this.btn_Login.TabIndex = 2;
+            this.btn_Login.Text = "Login";
+            this.btn_Login.UseVisualStyleBackColor = true;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // lbl_passWord
+            // 
+            this.lbl_passWord.AutoSize = true;
+            this.lbl_passWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_passWord.Location = new System.Drawing.Point(12, 149);
+            this.lbl_passWord.Name = "lbl_passWord";
+            this.lbl_passWord.Size = new System.Drawing.Size(167, 37);
+            this.lbl_passWord.TabIndex = 3;
+            this.lbl_passWord.Text = "Password:";
+            // 
+            // txtbox_passWord
+            // 
+            this.txtbox_passWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox_passWord.Location = new System.Drawing.Point(211, 149);
+            this.txtbox_passWord.Name = "txtbox_passWord";
+            this.txtbox_passWord.PasswordChar = '*';
+            this.txtbox_passWord.Size = new System.Drawing.Size(215, 38);
+            this.txtbox_passWord.TabIndex = 4;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(438, 364);
+            this.Controls.Add(this.txtbox_passWord);
+            this.Controls.Add(this.lbl_passWord);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.txtbox_userName);
+            this.Controls.Add(this.lbl_userName);
+            this.Name = "Login";
+            this.Text = "Login";
+            //this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_userName;
+        private System.Windows.Forms.TextBox txtbox_userName;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.Label lbl_passWord;
+        private System.Windows.Forms.TextBox txtbox_passWord;
+    }
+}
+
---
+++ LoginForm1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LoginForm
+{
+    public partial class Login : Form
+    {
+        //string to data source
+        string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\super\Documents\Visual Studio 2017\Projects\LoginForm1\LoginForm1\Login.mdf;Integrated Security=True";
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+   
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+            //set variables
+            string userName = txtbox_userName.Text;
+            string passWord = txtbox_passWord.Text;
+            //check user input
+            if (!string.IsNullOrEmpty(userName) && !string.IsNullOrEmpty(passWord))
+            {
+                login(userName, passWord);
+              
+            }
+            else
+            {
+                MessageBox.Show("You must enter your username and password");
+                return;
+            }
+              
+
+                    
+        }
+        private void login(string userName, string passWord)
+        {
+            //check database
+            using (SqlConnection sqlconn = new SqlConnection(ConnectionString))
+            {
+                sqlconn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM UsersTable WHERE USERNAME = @user AND PASSWORD =@pass", sqlconn);
+                cmd.Parameters.AddWithValue("@user", userName);
+                cmd.Parameters.AddWithValue("@pass", passWord);
+                int result = (int)(cmd.ExecuteScalar());
+
+                if (result > 0)
+                {
+                    MessageBox.Show(string.Format("Welcome: {0}", userName));
+                    MainPage form2 = new MainPage();
+                    form2.ShowDialog();
+
+                }
+                else
+                {
+                    MessageBox.Show("Login Failed");
+                    txtbox_passWord.Clear();
+                }
+            }
+        }
+    }
+}
